8c55d7578116060fe9847ea7133600fa7915f3d5
titosvq-mb <adlflpz@gmail.com>
2025-03-06 22:35:37 +0100

    Primer commit del roadmap

---
+++ #00/titosvq.py
@@ -0,0 +1,18 @@
+# URL Oficial de Python: https://www.python.org/
+
+"""
+Comentario de varias lineas
+Esta es la segunda linea
+Esta en la tercera linea
+y una cuarta linea
+"""
+
+cadena_str = "Esta variable es un string"
+edad_int = 36
+altura_float = 1.77
+var_bool = True
+colores_tuple = ("negro", "amarillo", "azul")
+lista_list = ["pera", "manzana", "platano", "fresa"]
+
+print("Hola, Python!")
+
---
+++ #01/titosvq.py
@@ -0,0 +1,17 @@
+suma = 3 + 4 # Suma
+print(suma) # Imprime la suma
+
+resta = 3 - 4 # Resta
+print(resta) # Imprime la resta
+
+multiplica = 3 * 4 # Multiplicación
+print(multiplica) # Imprime la multiplicación
+
+divide = 3 / 4 # División
+print(divide) # Imprime la división
+
+potencia = 3 ** 4 # Potencia
+print(potencia) # Imprime la potencia
+
+modulo = 12 % 5 # Módulo
+print(modulo) # Imprime el módulo
