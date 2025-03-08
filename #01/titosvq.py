a11dc131e5377af8199ec6e5317f46c8e0cde909
titosvq-mb <adlflpz@gmail.com>
2025-03-08 22:38:05 +0100

    Añadimos nuevos operadores

--- #01/titosvq.py
+++ #01/titosvq.py
@@ -1,3 +1,5 @@
+### Operadores aritméticos ###
+
 suma = 3 + 4 # Suma
 print(suma) # Imprime la suma
 
@@ -15,3 +17,27 @@ print(potencia) # Imprime la potencia
 
 modulo = 12 % 5 # Módulo
 print(modulo) # Imprime el módulo
+
+division_entera = 12 // 7 # División entera
+print(division_entera) # Imprime la división entera
+
+### Operadores de comparación ###
+
+print(3 == 3)
+
+print(1 != 2)
+
+print(1 > 2)
+
+print(1 < 2)
+
+print(1 >= 2)
+
+print(1 <= 2)
+
+### Operadores lógicos ###
+
+print("Ahora veamos otro tipo de operadores")
+
+print(4 == 4 and 4 < 5) 
+print(4 == 4 and 4 > 5) 
